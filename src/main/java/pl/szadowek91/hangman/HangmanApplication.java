d316c113f6d98e6517a86f536ce2bca48a312eab
Partyk Granda <szadowek91@gmail.com>
2022-10-23 12:04:31 +0200

    Initial commit

---
+++ src/main/java/pl/szadowek91/hangman/HangmanApplication.java
@@ -0,0 +1,13 @@
+package pl.szadowek91.hangman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmanApplication.class, args);
+	}
+
+}

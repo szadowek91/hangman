7294d12ab81aee62f201c6ce27fa0ca487bc99eb
PG <szadowek91@gmail.com>
2022-10-24 12:58:14 +0200

    httpsession done

--- src/main/java/pl/szadowek91/hangman/controller/mainCtrl.java
+++ src/main/java/pl/szadowek91/hangman/controller/mainCtrl.java
@@ -3,12 +3,9 @@ package pl.szadowek91.hangman.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import pl.szadowek91.hangman.service.WordService;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
@@ -27,11 +24,15 @@ public class mainCtrl {
         return "test";
     }
 
-    @GetMapping() // todo by using httpsession
-    public String game(Model model, String input, HttpServletRequest request, HttpServletResponse response, HttpSession session) throws ServletException, IOException {
-        String randomWord = wordService.selectRandomWord();
-        model.addAttribute("word", randomWord);
-        session.setAttribute("word", randomWord);
-        response.sendRedirect("hangman");
-        return "hangman";}
+    @GetMapping()
+    public String game(Model model, HttpSession session) throws ServletException, IOException {
+        if (session.getAttribute("word") != null) {
+            String randomWord = wordService.selectRandomWord();
+            session.setAttribute("word", randomWord);
+        }
+        String word = (String) session.getAttribute("word");
+        model.addAttribute("word", word); // at the end to remove (for review purposes)
+
+        return "hangman";
+    }
 }
